5a05a7620cffdba72e6c04eafe97604482a23a95
Ryan LaBouve <imryanlabouve@gmail.com>
2014-10-12 11:10:54 -0500

    Add options framework

--- functions.php
+++ functions.php
@@ -129,3 +129,26 @@ require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/jetpack.php';
 
 add_filter( 'show_admin_bar', '__return_false' );
+
+/*
+ * Helper function to return the theme option value. If no value has been saved, it returns $default.
+ * Needed because options are saved as serialized strings.
+ *
+ * This code allows the theme to work without errors if the Options Framework plugin has been disabled.
+ */
+
+if ( !function_exists( 'of_get_option' ) ) {
+	function of_get_option($name, $default = false) {
+		$optionsframework_settings = get_option('optionsframework');
+		// Gets the unique option id
+		$option_name = $optionsframework_settings['id'];
+		if ( get_option($option_name) ) {
+			$options = get_option($option_name);
+		}
+		if ( isset($options[$name]) ) {
+			return $options[$name];
+		} else {
+			return $default;
+		}
+	}
+}
