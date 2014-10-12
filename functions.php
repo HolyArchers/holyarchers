e5c55d663826ee32870d47bfef86fb2873606323
Ryan LaBouve <imryanlabouve@gmail.com>
2014-10-11 22:12:03 -0500

    Remove more underscores themes

--- functions.php
+++ functions.php
@@ -97,10 +97,6 @@ function holyarchers_scripts() {
 	wp_enqueue_style( 'holyarchers-main', get_template_directory_uri(). '/css/main.css', array(), '3.0.2');
 	wp_enqueue_style( 'holyarchers-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'holyarchers-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'holyarchers-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
