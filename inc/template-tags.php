bc968839fb53b0d46215f25e3ea88520fa03e403
Ryan LaBouve <imryanlabouve@gmail.com>
2014-10-13 11:23:03 -0500

    Random blogging updates

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -49,7 +49,7 @@ function holyarchers_post_nav() {
 	}
 	?>
 	<nav class="navigation post-navigation" role="navigation">
-		<h1 class="screen-reader-text"><?php _e( 'Post navigation', 'holyarchers' ); ?></h1>
+		<!-- <h1 class="screen-reader-text"><?php _e( 'Post navigation', 'holyarchers' ); ?></h1> -->
 		<div class="nav-links">
 			<?php
 				previous_post_link( '<div class="nav-previous">%link</div>', _x( '<span class="meta-nav">&larr;</span>&nbsp;%title', 'Previous post link', 'holyarchers' ) );
