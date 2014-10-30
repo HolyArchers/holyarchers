f785b67b5466a489c286dd3e7d3f9fedf416a405
Ryan LaBouve <imryanlabouve@gmail.com>
2014-10-30 15:41:55 -0500

    Add box on single pages and fix spacing in header

--- content-single.php
+++ content-single.php
@@ -3,27 +3,28 @@
  * @package holyarchers
  */
 ?>
+<div class="box box-main">
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			<div class="entry-meta">
+				<?php holyarchers_posted_on(); ?>
+			</div><!-- .entry-meta -->
+		</header><!-- .entry-header -->
 
-		<div class="entry-meta">
-			<?php holyarchers_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'holyarchers' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'holyarchers' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php holyarchers_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+		<footer class="entry-footer">
+			<?php holyarchers_entry_footer(); ?>
+		</footer><!-- .entry-footer -->
+	</article><!-- #post-## -->
+</div>
